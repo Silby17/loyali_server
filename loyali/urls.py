4ce7767a162915c6f9ef78b16a95768ee64d8200
Yosef Silberhaft <yossisilb@gmail.com>
2017-03-05 11:36:04 +0200

    added urls for users and saved page

--- loyali/urls.py
+++ loyali/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from views import index, login, admin_main, vendor_main, contact_us, logout,\
-    vendor_add, VendorAPI, add_admin_user, view_vendors
+    vendor_add, VendorAPI, admin_user_page, view_vendors, AdminUserAPI, saved_page, \
+    redirect_to_main
 
 urlpatterns = [
 
@@ -10,10 +11,14 @@ urlpatterns = [
 
     url(r'^admin/mainmenu', admin_main, name='admin_main'),
 
+    url(r'^redirect/mainmenu/', redirect_to_main, name='redirect_main'),
+
+    url(r'^admin/users/', AdminUserAPI.as_view(), name='admin_users'),
+
+    url(r'^admin/add_user/', admin_user_page, name='add_admin-user'),
+
     url(r'^admin/vendors', view_vendors, name='view_vendors'),
 
-    # POST will create a new Vendor user
-    # GET will return a list of all vendors
     url(r'^vendor/', VendorAPI.as_view()),
 
     url(r'^add/vendor', vendor_add, name='vendor_add'),
@@ -24,6 +29,5 @@ urlpatterns = [
 
     url(r'^logout/', logout, name='logout'),
 
-    url(r'^addUser/', add_admin_user, name='add_admin_user'),
-
+    url(r'^saved/', saved_page, name='saved'),
 ]

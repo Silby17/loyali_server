8dd8e84f45caf4c4d7786b60ce8eff134aad7ac0
Yosef Silberhaft <yossisilb@gmail.com>
2017-03-23 22:18:48 +0200

    Registration added

--- loyaliapi/urls.py
+++ loyaliapi/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from views import MobileUserAPI
 
 urlpatterns = [
+
+    url(r'^mobile/users/', MobileUserAPI.as_view(), name='mobile_user'),
 
 
 ]
--- loyaliapi/views.py
+++ loyaliapi/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth.models import Group
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.parsers import FormParser, MultiPartParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from loyaliapi.models import MobileUser
+from loyaliapi.serializer import MobileUserModelSerializer
+
+CUSTOMER_GROUP_NAME = 'customer'
+ADMIN_GROUP_NAME = 'admin'
+
+
+class MobileUserAPI(GenericAPIView):
+    queryset = MobileUser.objects.all()
+    serializer_class = MobileUserModelSerializer
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+        raw_data = request.POST.copy()
+        username = raw_data.get('username')
+
+        try:
+            customer_user = MobileUser.objects.get(username=username)
+            context = {"error": "user already exists"}
+            return Response(context, status=status.HTTP_409_CONFLICT)
+        except:
+            serializer = self.get_serializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                user = serializer.save()
+                group = Group.objects.get_or_create(name=CUSTOMER_GROUP_NAME)[0]
+                group.user_set.add(user)
+                context = {"user_created": "user created successfully"}
+                return Response(status=status.HTTP_200_OK)
+            else:
+                errors = ""
+                serializer_errors = serializer.erros
+                for error in serializer_errors:
+                    errors += serializer_errors[error][0]
+
+
+# This function will allow mobile users to log in to the app
+class CheckUserCredentials(APIView):
+    parser_classes = ([FormParser, MultiPartParser])
+
+    def post(self, request):
+        raw_data = request.POST.copy()
+        email = raw_data.get('email')
+        password = raw_data.get('password')
+        try:
+            # Checks if the user exists
+            user = MobileUser.objects.get(username=email)
+            auth_user = auth.authenticate(username=email, password=password)
+            if auth_user is not None:
+                print email, " - has logged in"
+                return Response(status=status.HTTP_200_OK)
+            else:
+                return Response(status=status.HTTP_401_UNAUTHORIZED)
+        except MobileUser.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)

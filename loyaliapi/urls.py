2ee4fde4580af8b691e79a835ae65a66176c8307
Yosef Silberhaft <yossisilb@gmail.com>
2017-03-29 16:12:29 +0300

    added mobile login URL

--- loyaliapi/urls.py
+++ loyaliapi/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from views import MobileUserAPI
+from views import MobileUserAPI, CheckUserCredentials
 
 urlpatterns = [
 
     url(r'^mobile/users/', MobileUserAPI.as_view(), name='mobile_user'),
 
+    url(r'^login/', CheckUserCredentials.as_view(), name='mobile_login'),
 
 ]

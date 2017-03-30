592d9e9811681bb9683b28ca325c691fa0227d98
Yosef Silberhaft <yossisilb@gmail.com>
2017-03-30 11:21:54 +0300

    Added method that will add a subscription

--- loyaliapi/views.py
+++ loyaliapi/views.py
@@ -5,6 +5,8 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from loyali.models import VendorUser, Subscription, Card, CardsInUse
 from loyaliapi.models import MobileUser
 from loyaliapi.serializer import MobileUserModelSerializer
 
@@ -63,3 +65,36 @@ class CheckUserCredentials(APIView):
         except MobileUser.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
+
+# This will create a new subscription between a customer and vendor
+class AddSubscription(APIView):
+    def post(self, request):
+        context = {}
+        try:
+            data = request.data
+            vendor_id = data.get('vendor_id')
+            customer_id = data.get('customer_id')
+            try:
+                vendor = VendorUser.objexts.get(id=vendor_id)
+            except:
+                context = {'error_message': 'Invalid Vendor ID'}
+                return Response(context, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                mobile_user = MobileUser.objects.get(id=customer_id)
+            except:
+                context = {'error_message': 'Invalid Customer ID'}
+                return Response(context, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                subscription = Subscription.objects.get(vendor=vendor_id, customer=customer_id)
+                context = {'error_message': 'Subscription already exists'}
+                return Response(context, status=status.HTTP_400_BAD_REQUEST)
+            except:
+                new_subscription = Subscription.objects.create(vendor=vendor_id, customer=customer_id)
+                vendor_cards = Card.objects.filter(vendor=vendor_id.vendor)
+                for card in vendor_cards:
+                    CardsInUse.objects.get_or_create(customer=customer_id, card=card, current=0)
+                    context = {'success_message': 'Subscription Created successfully'}
+                    return Response(context, status=status.HTTP_200_OK)
+        except:
+            context = {'error_message': 'Unknown error occurred'}
+            return Response(context, status=status.HTTP_400_BAD_REQUEST)

07063e59562fb94d32c4043e7d854be528ab0e61
Yosef Silberhaft <yossisilb@gmail.com>
2017-03-30 13:35:49 +0300

    Registered models to Admin site

--- loyali/admin.py
+++ loyali/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from loyali.models import Subscription, Card, CardsInUse, Rewards, Vendor, VendorUser
+from loyaliapi.models import MobileUser
+
+admin.site.register(Subscription)
+admin.site.register(Card)
+admin.site.register(CardsInUse)
+admin.site.register(Rewards)
+admin.site.register(Vendor)
+admin.site.register(VendorUser)
+admin.site.register(MobileUser)

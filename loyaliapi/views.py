97a39426798e5895488f52e88f68e7ba3580642d
Yosef Silberhaft <yossisilb@gmail.com>
2017-03-30 14:07:08 +0300

    Subscription Creation working

--- loyaliapi/views.py
+++ loyaliapi/views.py
@@ -5,7 +5,6 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.parsers import FormParser, MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from loyali.models import VendorUser, Subscription, Card, CardsInUse
 from loyaliapi.models import MobileUser
 from loyaliapi.serializer import MobileUserModelSerializer
@@ -25,7 +24,7 @@ class MobileUserAPI(GenericAPIView):
 
         try:
             customer_user = MobileUser.objects.get(username=username)
-            context = {"error": "user already exists"}
+            context = {"message": "user already exists"}
             return Response(context, status=status.HTTP_409_CONFLICT)
         except:
             serializer = self.get_serializer(data=request.data)
@@ -76,26 +75,32 @@ class AddSubscription(APIView):
             vendor_id = data.get('vendor_id')
             customer_id = data.get('customer_id')
             try:
-                vendor = VendorUser.objexts.get(id=vendor_id)
+                # Gets the VendorUser from the DB
+                vendor_user = VendorUser.objects.get(id=vendor_id)
             except:
-                context = {'error_message': 'Invalid Vendor ID'}
+                context = {'message': 'Invalid Vendor ID'}
                 return Response(context, status=status.HTTP_400_BAD_REQUEST)
             try:
+                # Gets the Customer from the DB
                 mobile_user = MobileUser.objects.get(id=customer_id)
             except:
-                context = {'error_message': 'Invalid Customer ID'}
+                context = {'message': 'Invalid Customer ID'}
                 return Response(context, status=status.HTTP_400_BAD_REQUEST)
             try:
+                # Checks to see if the Subscription already exists
                 subscription = Subscription.objects.get(vendor=vendor_id, customer=customer_id)
-                context = {'error_message': 'Subscription already exists'}
+                context = {'message': 'Subscription already exists'}
                 return Response(context, status=status.HTTP_400_BAD_REQUEST)
             except:
-                new_subscription = Subscription.objects.create(vendor=vendor_id, customer=customer_id)
-                vendor_cards = Card.objects.filter(vendor=vendor_id.vendor)
+                # Creates a subscription between Customer and Vendor
+                new_subscription = Subscription.objects.create(vendor=vendor_user, customer=mobile_user)
+                # Get all the cards of the Vendor
+                vendor_cards = Card.objects.filter(vendor=vendor_user.vendor)
+                # Assign the User the Cards
                 for card in vendor_cards:
-                    CardsInUse.objects.get_or_create(customer=customer_id, card=card, current=0)
-                    context = {'success_message': 'Subscription Created successfully'}
-                    return Response(context, status=status.HTTP_200_OK)
+                    CardsInUse.objects.create(customer=mobile_user, card=card, current=0)
+                context = {'message': 'Subscription Created successfully'}
+                return Response(context, status=status.HTTP_200_OK)
         except:
-            context = {'error_message': 'Unknown error occurred'}
+            context = {'message': 'Unknown error occurred'}
             return Response(context, status=status.HTTP_400_BAD_REQUEST)

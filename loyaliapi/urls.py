2c63009fe4bdf426a8aee119ac9892e40565df30
Yosef Silberhaft <yossisilb@gmail.com>
2017-03-30 20:21:12 +0300

    added url for getting vendor by ID

--- loyaliapi/urls.py
+++ loyaliapi/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import url
-from views import MobileUserAPI, CheckUserCredentials, AddSubscription
+from views import MobileUserAPI, CheckUserCredentials, \
+    AddSubscription, CustomerSubscriptionAPI, VendorByIDAPI
 
 urlpatterns = [
 
-    url(r'^mobile/users/', MobileUserAPI.as_view(), name='mobile_user'),
-
     url(r'^login/', CheckUserCredentials.as_view(), name='mobile_login'),
 
+    url(r'^mobile/users/', MobileUserAPI.as_view(), name='mobile_user'),
+
+    url(r'^mobile/subscriptions/', CustomerSubscriptionAPI.as_view(), name='customer_subscriptions'),
+
+    url(r'^mobile/vendorByID/', VendorByIDAPI.as_view(), name='vendor_by_id'),
+
     url(r'^createSubscription/', AddSubscription.as_view(), name='create_subscription'),
+
+
 
 ]

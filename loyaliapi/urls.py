eb5cff403a3aa3ceb4f8c6a53c952708268f07b3
Yosef Silberhaft <yossisilb@gmail.com>
2017-03-30 14:07:31 +0300

    added URL to create Subscription

--- loyaliapi/urls.py
+++ loyaliapi/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
-from views import MobileUserAPI, CheckUserCredentials
+from views import MobileUserAPI, CheckUserCredentials, AddSubscription
 
 urlpatterns = [
 
     url(r'^mobile/users/', MobileUserAPI.as_view(), name='mobile_user'),
 
     url(r'^login/', CheckUserCredentials.as_view(), name='mobile_login'),
+
+    url(r'^createSubscription/', AddSubscription.as_view(), name='create_subscription'),
 
 ]

39b2bd74d8b145e1e329b35eb7fa7f78c0e6ba78
Yosef Silberhaft <yossisilb@gmail.com>
2017-05-18 18:55:13 +0300

    Added display of details

--- loyali/admin.py
+++ loyali/admin.py
@@ -5,9 +5,25 @@ from loyali.models import Subscription, Card, CardsInUse, Rewards, Vendor, Vendo
 from loyaliapi.models import MobileUser
 
 admin.site.register(Subscription)
-admin.site.register(Card)
-admin.site.register(CardsInUse)
+
 admin.site.register(Rewards)
-admin.site.register(Vendor)
 admin.site.register(VendorUser)
 admin.site.register(MobileUser)
+
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'store_name')
+
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'max', 'description')
+
+
+class CardsInUseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'card', 'current')
+
+
+admin.site.register(Card, CardAdmin)
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(CardsInUse, CardsInUseAdmin)
+

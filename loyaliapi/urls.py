3a35cd577f1660b4f0bb240d750f0c06189db25b
Yosef Silberhaft <yossisilb@gmail.com>
2017-05-18 18:55:59 +0300

    Added new URL's

--- loyaliapi/urls.py
+++ loyaliapi/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from views import MobileUserAPI, CheckUserCredentials, AddSubscription, VendorByIDAPI, \
     VendorWithCards, CustomersCards, SubscriptionsWithCardsInUse, \
-    SubscriptionCardsByVendorID, TestingAPI
+    SubscriptionCardsByVendorID, TestingAPI, DeleteSubscriptionAPI
 
 urlpatterns = [
 
@@ -11,12 +11,13 @@ urlpatterns = [
 
     url(r'^mobile/subscriptions/', SubscriptionsWithCardsInUse.as_view(), name='customer_subscriptions'),
 
-
     url(r'^mobile/users/', MobileUserAPI.as_view(), name='mobile_user'),
 
     url(r'^mobile/vendorByID/', VendorByIDAPI.as_view(), name='vendor_by_id'),
 
     url(r'^createSubscription/', AddSubscription.as_view(), name='create_subscription'),
+
+    url(r'^deleteSubscription/', DeleteSubscriptionAPI.as_view(), name='delete_subscriptions'),
 
     url(r'^mobile/vendorsAndCards/', VendorWithCards.as_view(), name='customers_vendors_and_cards'),
 

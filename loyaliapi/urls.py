b6178292056be640680e4590b52a64691b745b7f
Yosef Silberhaft <yossisilb@gmail.com>
2017-05-21 14:10:23 +0300

    Added URL for Rewards in API

--- loyaliapi/urls.py
+++ loyaliapi/urls.py
@@ -1,27 +1,31 @@
 from django.conf.urls import url
-from views import MobileUserAPI, CheckUserCredentials, AddSubscription, VendorByIDAPI, \
-    VendorWithCards, CustomersCards, SubscriptionsWithCardsInUse, \
-    SubscriptionCardsByVendorID, TestingAPI, DeleteSubscriptionAPI, PunchCardAPI
+from views import MobileUserAPI, CheckUserCredentialsAPI, AddSubscriptionAPI, \
+    VendorByIdAPI, \
+    VendorsWithCardsAPI, CustomersCardsAPI, AllCustomersSubscriptionsAPI, \
+    SingleSubscriptionAPI, TestingAPI, DeleteSubscriptionAPI, PunchCardAPI, \
+    CustomerRewardsAPI
 
 urlpatterns = [
 
-    url(r'^login/', CheckUserCredentials.as_view(), name='mobile_login'),
+    url(r'^login/', CheckUserCredentialsAPI.as_view(), name='mobile_login'),
 
-    url(r'^mobile/subscriptions/byCardID/', SubscriptionCardsByVendorID.as_view(), name='customer_subscriptions_by_card'),
+    url(r'^mobile/subscriptions/byCardID/', SingleSubscriptionAPI.as_view(), name='customer_subscriptions_by_card'),
 
-    url(r'^mobile/subscriptions/', SubscriptionsWithCardsInUse.as_view(), name='customer_subscriptions'),
+    url(r'^mobile/subscriptions/', AllCustomersSubscriptionsAPI.as_view(), name='customer_subscriptions'),
 
     url(r'^mobile/users/', MobileUserAPI.as_view(), name='mobile_user'),
 
-    url(r'^mobile/vendorByID/', VendorByIDAPI.as_view(), name='vendor_by_id'),
+    url(r'^mobile/vendorByID/', VendorByIdAPI.as_view(), name='vendor_by_id'),
 
-    url(r'^createSubscription/', AddSubscription.as_view(), name='create_subscription'),
+    url(r'^createSubscription/', AddSubscriptionAPI.as_view(), name='create_subscription'),
 
     url(r'^deleteSubscription/', DeleteSubscriptionAPI.as_view(), name='delete_subscriptions'),
 
-    url(r'^mobile/vendorsAndCards/', VendorWithCards.as_view(), name='customers_vendors_and_cards'),
+    url(r'^mobile/vendorsAndCards/', VendorsWithCardsAPI.as_view(), name='customers_vendors_and_cards'),
 
-    url(r'^mobile/customerCards/', CustomersCards.as_view(), name='customers_cards'),
+    url(r'^mobile/rewards/', CustomerRewardsAPI.as_view(), name='customer_rewards'),
+
+    url(r'^mobile/customerCards/', CustomersCardsAPI.as_view(), name='customers_cards'),
 
     url(r'^mobile/punchCard/', PunchCardAPI.as_view(), name='punch_card'),
 
